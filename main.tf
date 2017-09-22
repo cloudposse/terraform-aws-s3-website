9ef6df55fe379a11d847a9248f88a988ac1ccbef
Erik Osterman <erik@cloudposse.com>
2017-09-22 07:22:00 -0700

    Fix cors_allowed_origins (#8)

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket" "default" {
   cors_rule {
     allowed_headers = "${var.cors_allowed_headers}"
     allowed_methods = "${var.cors_allowed_methods}"
-    allowed_origins = "${var.cors_allowed_origins}"
+    allowed_origins = ["${var.cors_allowed_origins}"]
     expose_headers  = "${var.cors_expose_headers}"
     max_age_seconds = "${var.cors_max_age_seconds}"
   }
