fd49440c84cf061cc7ea6f241c96f1a7475d938b
Andriy Knysh <aknysh@users.noreply.github.com>
2018-05-10 18:48:00 -0400

    Add `website_endpoint` output. Add TravisCI (#18)

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "hostname" {
+  value = "${var.hostname}"
+}
+
 output "s3_bucket_name" {
   value = "${aws_s3_bucket.default.id}"
 }
@@ -6,6 +10,18 @@ output "s3_bucket_domain_name" {
   value = "${aws_s3_bucket.default.bucket_domain_name}"
 }
 
-output "hostname" {
-  value = "${var.hostname}"
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.default.arn}"
+}
+
+output "s3_bucket_website_endpoint" {
+  value = "${aws_s3_bucket.default.website_endpoint}"
+}
+
+output "s3_bucket_website_domain" {
+  value = "${aws_s3_bucket.default.website_domain}"
+}
+
+output "s3_bucket_hosted_zone_id" {
+  value = "${aws_s3_bucket.default.hosted_zone_id}"
 }
