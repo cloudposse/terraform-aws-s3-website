1259124c7569c1a4307de066e248c3cfd0c3441c
rverma-nikiai <rohit.verma@niki.ai>
2019-06-23 07:11:20 +0530

    referencing to rverma-nikiai for the time cloudposse updates didn't finished

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ locals {
 }
 
 module "logs" {
-  source                   = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=master"
+  source                   = "git::https://github.com/rverma-nikiai/terraform-aws-s3-log-storage.git?ref=master"
   name                     = var.name
   stage                    = var.stage
   namespace                = var.namespace
@@ -247,7 +247,7 @@ data "aws_iam_policy_document" "deployment" {
 }
 
 module "dns" {
-  source = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=master"
+  source = "git::https://github.com/rverma-nikiai/terraform-aws-route53-alias.git?ref=master"
   aliases = compact(
     [
       signum(length(var.parent_zone_id)) == 1 || signum(length(var.parent_zone_name)) == 1 ? var.hostname : "",
