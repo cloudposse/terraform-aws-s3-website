87c122ec2e1af3a5da325cc10acaaedd6a1f2126
rverma-nikiai <rohit.verma@niki.ai>
2019-06-26 18:10:59 +0530

    upgraded 0.12

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "logs" {
 }
 
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.1"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
