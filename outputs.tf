ee9e51307332b0988fc4f861379f980a4b5f5d4a
Maxim Mironenko <maxim@cloudposse.com>
2020-03-04 11:05:53 -0800

    Fix typo (#30)
    * Fix typo
    
    * Fix typo
    
    * Updated README.md
    
    Co-authored-by: Maxim Mironenko <simixido@gmail.com>
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -10,12 +10,12 @@ output "s3_bucket_name" {
 
 output "s3_bucket_domain_name" {
   value       = aws_s3_bucket.default.bucket_domain_name
-  description = "Name of of website bucket"
+  description = "Name of website bucket"
 }
 
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.default.arn
-  description = "Name of of website bucket"
+  description = "ARN identifier of website bucket"
 }
 
 output "s3_bucket_website_endpoint" {
