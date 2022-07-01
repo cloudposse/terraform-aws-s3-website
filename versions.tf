c05716428ce691ecfbc2f83f534dd01b0124ef3a
John C. Bland II <johncblandii@gmail.com>
2022-07-01 16:52:12 -0500

    chore: upgrade packages to support AWS v4
    Closes #75

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0, < 4.0.0"
+      version = ">= 4.9.0"
     }
     local = {
       source  = "hashicorp/local"
